32e0d93dc397bcd12a7ab422193c93c0fa0c9dba
20240614-menna <20240614@stud.fci-cu.edu.eg>
2026-04-29 03:06:07 +0300

    Create EmployeeView.java

---
+++ EmployeeView.java
@@ -0,0 +1,8 @@
+class EmployeeView {
+    public void printEmployeeDetails(Employee e) {
+        System.out.println("Employee:");
+        System.out.println("ID: " + e.getId());
+        System.out.println("Name: " + e.getName());
+        System.out.println("Email: " + e.getEmail());
+    }
+}
